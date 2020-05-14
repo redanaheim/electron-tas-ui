10e4899ccf2ee818673a4d435fa42e7be85540ae
TigerGold59 <Tigerball890@gmail.com>
2020-05-14 18:32:09 -0400

    Create basic functionality on export page
    TODO: Make values stored from inputs

--- src/exporting/index.ts
+++ src/exporting/index.ts
@@ -1,10 +1,62 @@
+import { desktopCapturer } from "electron";
+import { IpAddr } from "../ftp";
+import { Store } from "../storing";
 var path: any = null;
 const { dialog, BrowserWindow } = require("electron").remote;
 const pick_file = async function (): Promise<string> {
-  path = await dialog.showOpenDialog(BrowserWindow.getFocusedWindow(), {
-    properties: ["openFile"],
-  });
-  $("#file_path").val(path.filePaths[0] || "");
+  path = (
+    await dialog.showOpenDialog(BrowserWindow.getFocusedWindow(), {
+      properties: ["openFile"],
+    })
+  ).filePaths[0];
+  $("#file_path").val(path || "");
   return path;
 };
-module.exports = pick_file;
+const send = async function (): Promise<void> {
+  const this_window = BrowserWindow.getFocusedWindow();
+  let ip_text = $("#switch_ip").val().toString();
+  let name = $("#path_on_switch").val().toString();
+
+  let switch_ip = new IpAddr(ip_text);
+  let is_replacing: boolean;
+  try {
+    is_replacing = await switch_ip.exists("/scripts/", name, 5000);
+  } catch (err) {
+    await dialog.showMessageBox(this_window, {
+      message: err.toString(),
+      type: "error",
+      buttons: ["OK"],
+    });
+    return;
+  }
+  if (is_replacing) {
+    let should_replace = await dialog.showMessageBox(this_window, {
+      message: `This will overwrite /scripts/${name} on the Switch. The current file will be backed up, however do you still want to proceed?`,
+      type: "question",
+      buttons: ["Cancel", "Replace"],
+    });
+    if (should_replace.response === 0) {
+      // User clicked cancel
+      return;
+    }
+  }
+  let result;
+  try {
+    result = await switch_ip.send(path, name, 5000);
+  } catch (err) {
+    await dialog.showMessageBox(this_window, {
+      message: err.toString(),
+      type: "error",
+      buttons: ["OK"],
+    });
+    return;
+  }
+  dialog.showMessageBox(this_window, {
+    message: result.did_succeed
+      ? "Sucessfully exported script to Switch."
+      : "Export failed.",
+    type: "info",
+    buttons: ["OK"],
+  });
+};
+module.exports = { pick_file: pick_file, send: send };
---
+++ src/ftp.ts
@@ -0,0 +1,214 @@
+const Client = require("ftp");
+const fs = require("fs");
+const electron = require("electron");
+const format_time = function (): string {
+  let date = new Date();
+  let weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][
+    date.getDay()
+  ];
+  return `${weekday} ${date.getHours()}꞉${date.getMinutes()}꞉${date.getSeconds()}`;
+};
+interface PathTo<T> {
+  path: string;
+  internal_data?: T;
+}
+class Result {
+  did_succeed: boolean;
+  did_replace: boolean;
+  switch_ip: IpAddr;
+  old_file?: PathTo<string>;
+  constructor(
+    did_suceed?: boolean,
+    did_replace?: boolean,
+    switch_ip?: IpAddr,
+    old_file?: PathTo<string>
+  ) {
+    this.did_succeed = did_suceed;
+    this.did_replace = did_replace;
+    this.switch_ip = switch_ip;
+    this.old_file = old_file;
+  }
+}
+export class IpAddr {
+  static regex = /^((([0-2][0-5]?[0-5]?|[0-1][0-9]?[0-9]?)|[0-9][0-9])|[0-9])\.((([0-2][0-5]?[0-5]?|[0-1][0-9]?[0-9]?)|[0-9][0-9])|[0-9])\.((([0-2][0-5]?[0-5]?|[0-1][0-9]?[0-9]?)|[0-9][0-9])|[0-9])\.((([0-2][0-5]?[0-5]?|[0-1][0-9]?[0-9]?)|[0-9][0-9])|[0-9])$/;
+  parts: number[];
+  did_succeed: boolean;
+  error: string;
+  constructor(text: string) {
+    if (IpAddr.regex.test(text)) {
+      this.parts = text.split(".").map((x) => Number(x));
+    } else {
+      this.error =
+        "Invalid IP address format. Try 4 numbers between 0 and 255 separated by dots.";
+      this.parts = [0];
+      this.did_succeed = false;
+    }
+  }
+  get_connnect_obj(port?: number): any {
+    return {
+      host: this.parts.join("."),
+      port: port || 5000,
+    };
+  }
+  async backup(
+    target_path: string,
+    backup_name: string,
+    port: number,
+    connection?: any,
+    connection_ready?: boolean
+  ): Promise<PathTo<string>> {
+    return new Promise((res, rej) => {
+      if (connection) {
+        var client = connection;
+      } else {
+        var client = new Client();
+      }
+      let on_ready = function () {
+        let result: PathTo<string>;
+        client.get(target_path, (err: any, data: any) => {
+          if (err) {
+            rej(err);
+          }
+          let app_path: string;
+          if (electron.app) {
+            app_path = electron.app.getPath("userData");
+          } else {
+            app_path = electron.remote.app.getPath("userData");
+          }
+          if (fs.existsSync(app_path + "/backups/" + backup_name) === false) {
+            fs.writeFile(app_path + "/backups/" + backup_name, "", (err) => {
+              if (err) rej(err);
+            });
+          }
+          try {
+            data.pipe(
+              fs.createWriteStream(app_path + "/backups/" + backup_name)
+            );
+          } catch (err) {
+            rej(err);
+          }
+          data.once("close", () => {
+            res({
+              path: app_path + "/backups/" + backup_name,
+            });
+            if (connection_ready === false) {
+              client.end();
+            }
+          });
+        });
+      };
+      if (connection_ready === false) {
+        client.on("ready", on_ready);
+      } else {
+        on_ready();
+      }
+      if (!connection) {
+        try {
+          client.connect(this.get_connnect_obj(port));
+        } catch (err) {
+          rej(err);
+        }
+      }
+    });
+  }
+  async exists(
+    directory: string,
+    target: string,
+    port: number,
+    connection?: any,
+    connection_ready?: boolean
+  ): Promise<boolean> {
+    return new Promise((res, rej) => {
+      if (connection) {
+        var client = connection;
+      } else {
+        var client = new Client();
+      }
+      let on_ready = function () {
+        let result: Result;
+        client.list(directory, (err: any, dir: any[]) => {
+          if (err) {
+            rej(err);
+            client.end();
+          }
+          let exists = false;
+          dir.forEach((item: any) => {
+            if (item.name === target && item.type === "-") exists = true;
+          });
+          res(exists);
+          if (connection_ready === false) {
+            client.end();
+          }
+        });
+      };
+      if (connection_ready === false) {
+        client.on("ready", on_ready);
+      } else {
+        on_ready();
+      }
+      if (!connection) {
+        try {
+          client.connect(this.get_connnect_obj(port));
+        } catch (err) {
+          rej(err);
+        }
+      }
+    });
+  }
+  async send(source: string, target: string, port: number): Promise<Result> {
+    let that = this;
+    return new Promise((res, rej) => {
+      let connection = new Client();
+      let target_path = "/scripts/" + target;
+
+      let on_ready = async function () {
+        let result = new Result(false, false, new IpAddr("0.0.0.0"), {
+          path: "null",
+        });
+        // See if file exists
+        try {
+          result.did_replace = await that.exists(
+            "/scripts/",
+            target,
+            port,
+            connection,
+            true
+          );
+        } catch (err) {
+          console.error(err);
+          rej(err);
+        }
+        if (result.did_replace) {
+          try {
+            result.old_file = await that.backup(
+              target_path,
+              target + " " + format_time(),
+              port,
+              connection,
+              true
+            );
+          } catch (err) {
+            console.error(err);
+            rej(err);
+          }
+        }
+        // Backup file we will replace
+        connection.put(source, target_path, (err: any) => {
+          if (err) {
+            rej(err);
+          }
+          result.did_succeed = true;
+          result.switch_ip = that;
+          connection.end();
+          res(result);
+        });
+      };
+      connection.on("ready", on_ready);
+      try {
+        connection.connect(that.get_connnect_obj(5000));
+      } catch (err) {
+        rej(err);
+      }
+    });
+  }
+}
