2b921332322cc6143b7cd078d67191219523feeb
TigerGold59 <Tigerball890@gmail.com>
2020-06-08 21:04:33 -0400

    Make decompiler much better at its job

--- src/assets/compiling/decompile.ts
+++ src/assets/compiling/decompile.ts
@@ -52,6 +52,7 @@ class PureInputLine {
   on_keys: KeysList;
   off_keys: KeysList;
   clone_keys: KeysList;
+  are_keys_same: boolean;
   lstick_pos_cartesian: [number, number];
   lstick_pos: StickPos;
   lstick_changes: boolean;
@@ -66,6 +67,8 @@ class PureInputLine {
     return new PureInputLine("0 NONE 0;0 0;0");
   })();
   constructor(line: string, used_to_clear?: boolean, throw_errors?: boolean) {
+    this.on_keys = new KeysList();
+    this.off_keys = new KeysList();
     if (used_to_clear) this.used_to_clear = true;
     line = line.trim();
     if (
@@ -113,41 +116,45 @@ class PureInputLine {
     for (const key of all_keys) {
       const this_has = this.pressed_keys.get_array().includes(key);
       const prev_has = previous.pressed_keys.get_array().includes(key);
-      if (this_has === prev_has) {
+      if (this_has === prev_has && this_has === true) {
         this.clone_keys.append(key);
+      } else if (this_has === prev_has && this_has === false) {
+        continue;
       } else if (this_has) {
         this.on_keys.append(key);
       } else {
         this.off_keys.append(key);
       }
     }
+    console.log("this: ", this);
+    this.are_keys_same =
+      this.on_keys.equals(this.previous.on_keys) &&
+      this.off_keys.equals(this.previous.off_keys);
     this.lstick_changes = this.lstick_pos.equals(previous.lstick_pos) === false;
     this.rstick_changes = this.rstick_pos.equals(previous.rstick_pos) === false;
   }
   equals_last(): boolean {
-    const should_print_keys =
-      this.clone_keys.equals(this.pressed_keys) === false;
+    // Does this object have the exact same inputs as the one before it?
     return (
       this.lstick_changes === false &&
       this.rstick_changes === false &&
-      should_print_keys === false
+      this.are_keys_same
     );
   }
   print(): string {
     let buffer = `${this.frame === 1 ? "+" : this.frame - this.previous.frame}`;
-    const should_print_keys =
-      this.clone_keys.equals(this.pressed_keys) === false;
-    if (should_print_keys) {
+    // Should we add anything to the line regarding the keys?
+    if (this.are_keys_same === false) {
+      const on_keys = this.on_keys.get_array();
+      const off_keys = this.off_keys.get_array();
       if (this.pressed_keys.get_array().length === 0) {
         buffer += " OFF{ALL}";
+      } else if (this.clone_keys.get_array().length === 0) {
+        buffer += ` RAW{${this.pressed_keys.get_array().join(",")}}`;
       } else {
-        if (this.clone_keys.get_array().length === 0) {
-          buffer += ` RAW{${this.pressed_keys.get_array().join(",")}}`;
-        } else {
-          buffer += ` ON{${this.on_keys.get_array().join(",")}}`;
-          if (this.off_keys.get_array().length > 0) {
-            buffer += ` OFF{${this.off_keys.get_array().join(",")}}`;
-          }
+        if (on_keys.length > 0) buffer += ` ON{${on_keys.join(",")}}`;
+        if (off_keys.length > 0) {
+          buffer += ` OFF{${off_keys.join(",")}}`;
         }
       }
     }
@@ -169,25 +176,35 @@ export const decompile = function (
   script = typeof script === "string" ? script : "";
   const lines = script.split("\n").map((x) => x.replace(/[\r\n]/g, ""));
   const queue = [PureInputLine.empty];
+  let last_updated_frame = 0;
   for (const line of lines) {
     const obj = new PureInputLine(line);
+    let added_clear = false;
+    // Compare the current line to the last item in the queue
     obj.compare_to(queue[last_index_of(queue)]);
-    if (obj.frame - obj.previous.frame > 1) {
+    // This inserts a line right after the one before the one we are on right now to clear the inputs.
+    if (obj.frame - last_updated_frame > 1) {
+      added_clear = true;
       const new_empty_obj = new PureInputLine(
-        `${obj.previous.frame + 1} NONE 0;0 0;0`,
+        `${last_updated_frame + 1} NONE 0;0 0;0`,
         true
       );
-      new_empty_obj.compare_to(queue[last_index_of(queue)]);
-      queue.push(new_empty_obj);
+      new_empty_obj.compare_to(obj.previous);
+      if (new_empty_obj.equals_last() === false) {
+        queue.push(new_empty_obj);
+      }
     }
-    if (obj.equals_last() === false) {
+    // If it's equal to the last one in the queue anyway, why bother?
+    if (obj.equals_last() === false || added_clear) {
       queue.push(new PureInputLine(line, false, throw_errors || false));
     }
+    last_updated_frame = obj.frame;
   }
   for (let i = 1; i < queue.length; i++) {
     const element = queue[i];
     element.compare_to(queue[i - 1]);
     buffer += `${element.print()}\n`;
   }
+  console.log("queue: ", queue);
   return buffer;
 };
